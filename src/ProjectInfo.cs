90515750bd4554a5bc0b7539c23c6909892305c8
Christopher Banwarth <github@aprettycoolprogram.com>
2026-02-14 14:51:24 -0500

    Initial commit

---
+++ src/ProjectInfo.cs
@@ -0,0 +1,16 @@
+﻿// =============================================================================
+// Scruppy
+// A utility for Arc Raiders
+// https://github.com/APrettyCoolProgram/Scruppy
+// Copyright (c) A Pretty Cool Program. All rights reserved.
+// Licensed under the Apache 2.0 license.
+// -----------------------------------------------------------------------------
+// R26.2
+// =============================================================================
+
+namespace Scruppy;
+
+internal class ProjectInfo
+{
+    // This class is only used for informational purposes, and does not contain executable code.
+}
